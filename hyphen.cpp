de1f397398e156680854a5a9ae6caa9a86f7eb04
Justin Yedinak <justin.yedinak.19@cnu.edu>
2021-09-27 11:16:55 -0400

    thinking in terms of cases next

--- hyphen.cpp
+++ hyphen.cpp
@@ -4,20 +4,34 @@
 
 using namespace std;
 
-bool isVowel(char c){
+bool isVowel(char c) {
     c = tolower(c);
     return isalpha(c) && (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y');
 }
 
-bool isConsonant(char c){
+bool isConsonant(char c) {
     if (isalpha(c))
         return !isVowel(c);
     return false;
 }
 
-std::map<int, int> indivisible_positions(const char *c){
+bool isCluster(std::map<int, int> cluster_positions, int current_position){
+    return cluster_positions.find(current_position) != cluster_positions.end();
+}
 
-    std::string list[] = {"qu","tr","br","st","sl","bl","cr","ph","ch","str"};
+string getCluster(std::map<int, int> cluster_positions, int current_position, const char *c){
+    string str = "";
+    // Skip past cluster
+    int cluster_length = cluster_positions[current_position];
+    for (int j = 0; j < cluster_length; j++) {
+        str += *(c + j); // I think this is right???
+    }
+    return str;
+}
+
+std::map<int, int> indivisible_positions(const char *c) {
+
+    std::string list[] = {"qu", "tr", "br", "st", "sl", "bl", "cr", "ph", "ch", "str"};
     int length = list->length();
     string txt = c;
     std::transform(txt.begin(), txt.end(), txt.begin(), ::tolower);
@@ -43,58 +57,88 @@ char *process(const char *input) {
 
     string org_str = input;
     string new_str = "";
+    string group = "";
 
+    char previous;
     char current;
-    bool does_cur_begin_cluster;
-
     char next;
-    bool does_next_begin_cluster;
+    char next_next;
 
     bool is_prev_vowel = false;
     bool is_prev_consonant = false;
 
-    for (int i = 0; i < length; i++){
+    for (int i = 0; i < length; i++) {
         current = *(input + i);
-        does_cur_begin_cluster = cluster_positions.find(i) != cluster_positions.end();
 
         // Check if current is a letter, else skip
         if (isalpha(current)) {
-            // isVowel current and is there two more characters in the string, if yes then a pattern is possible
-            if (isVowel(current) && i + 2 < length){
-                next = *(input + i + 1);
-                does_next_begin_cluster = cluster_positions.find(i + 1) != cluster_positions.end();
-                if (isConsonant(next)) {
 
+
+            if (isVowel(current) && i + 2 < length) {
+                // true, then make it previous
+                // prev=V
+                is_prev_vowel = isVowel(current);
+                previous = current;
+                group += previous;
+
+
+                current = *(input + ++i);
+                // current=?
+                if (isConsonant(current)) {
+                    // Check if cluster
+                    if (isCluster(cluster_positions, i)) {
+                        // Skip past cluster
+                        int cluster_length = cluster_positions[i];
+                        for (int j = 0; j < cluster_length; j++) {
+                            group += *(input + i++); // I think this is right???
+                        }
+                    }
+
+                    // Check next
+                    next = *(input + i + 1);
+                    if (isalpha(next)) {
+
+                        if (isVowel(next)) {
+                            // vowel = true
+                            // found Pattern !!!
+                            cout << "pattern found";
+                            group += previous + "-" + current + next;
+                        } else {
+                            // vowel = false; must be consonant
+                            // time to check next_next
+                            if (i + 3 < length) {
+                                next_next = *(input + 3);
+
+                                if (isConsonant(current)) {
+
+                                    // Check if cluster
+                                    if (isCluster(cluster_positions, i)) {
+
+                                        // Skip past cluster
+//                                        group += getCluster();
+                                        int cluster_length = cluster_positions[i];
+                                        for (int j = 0; j < cluster_length; j++) {
+                                            group += *(input + i++); // I think this is right???
+                                        }
+                                        // Pattern found
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+
+                    // end
                 }
-
-                // end
-                is_prev_vowel = true;
-                is_prev_consonant = false;
             }
-            // current isConsonant
-            else {
-                // check if not a cluster
-                if (! (cluster_positions.find(i) != cluster_positions.end())){
-
-
-                }
-                // current is a cluster, skip ahead
-                else {
-                    i += cluster_positions[i];
-
-                }
-                // end
-                is_prev_vowel = false;
-                is_prev_consonant = true;
-            }
-
         } else {
             // Not a letter so both must be false
+            new_str += current;
             is_prev_vowel = false;
             is_prev_consonant = false;
         }
-    }
 
-    return NULL;
+        return NULL;
+    }
 }
 

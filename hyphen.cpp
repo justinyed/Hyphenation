8c3f0a7ce719fb15681bab80021536f9107a8b5b
Justin Yedinak <justin.yedinak.19@cnu.edu>
2021-09-27 15:52:57 -0400

    created functions to load and unload clusters in a string

--- hyphen.cpp
+++ hyphen.cpp
@@ -4,60 +4,28 @@
 
 using namespace std;
 
-bool isVowel(char c) {
-    c = tolower(c);
-    return isalpha(c) && (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y');
+// CONSTANTS
+const string SYM = "*";
+
+const regex REGEX_SYMBOL("[" + SYM + "]");
+const regex REGEX_VOWELS("[aeiouy]", std::regex::icase);
+const regex REGEX_CONSONANTS("[b-df-hj-np-tv-z]|[" + SYM + "]", std::regex::icase);
+const regex REGEX_CLUSTERS("(qu)|(tr)|(br)|(st)|(sl)|(bl)|(cr)|(ph)|(ch)|(str)", std::regex::icase);
+
+const regex REGEX_PATTERN_VCV("vcv", std::regex::icase);
+const regex REGEX_PATTERN_VCCV("vccv", std::regex::icase);
+
+string unload_clusters(string str){
+    return std::regex_replace(str, REGEX_CLUSTERS, SYM);
 }
 
-bool isConsonant(char c) {
-    if (isalpha(c))
-        return !isVowel(c);
-    return false;
+string load_clusters(string str, vector<string> found_clusters){
+    for (int i = 0; i < found_clusters.size(); i++)
+        str = std::regex_replace(str, REGEX_SYMBOL, found_clusters[i], std::regex_constants::format_first_only);
+    return str;
 }
-
-bool isCluster(std::map<int, int> cluster_positions, int current_position){
-    return cluster_positions.find(current_position) != cluster_positions.end();
-}
-
-//std::map<int, int> indivisible_positions(const char *c) {
-//
-//    std::vector<std::string> indivisible_positions = { };
-//    std::string list[] = {"qu", "tr", "br", "st", "sl", "bl", "cr", "ph", "ch", "str"};
-//    int length = list->length();
-//
-//    // convert to lower case
-//    string txt = c;
-//
-//
-//
-//    for (int i = 0; i < length; i++) {
-//        int found = txt.find(list[i]);
-//
-//        while (found != string::npos) {
-//            m[found] = list[i].length();
-//            found = txt.find(list[i], found + 1);
-//        }
-//    }
-//
-//    return m;
-//}
-
-
-
 
 char *process(const char *input) {
-
-    const string SYM = "*";
-    regex vowels("[aeiouy]", std::regex::icase);
-    regex consonants("[b-df-hj-np-tv-z]|[" + SYM + "]", std::regex::icase);
-    regex clusters("(qu)|(tr)|(br)|(st)|(sl)|(bl)|(cr)|(ph)|(ch)|(str)", std::regex::icase);
-    regex symbol("[" + SYM + "]");
-
-
-    regex vcv("vcv", std::regex::icase);
-    regex vccv("vccv", std::regex::icase);
-
-
 
     string org_str = input;
     string new_str = input;
@@ -67,7 +35,7 @@ char *process(const char *input) {
     vector<int> found_cluster_positions;
     vector<std::string> found_clusters;
 
-    auto it = std::sregex_iterator(new_str.begin(), new_str.end(), clusters);
+    auto it = std::sregex_iterator(new_str.begin(), new_str.end(), REGEX_CLUSTERS);
 
     for(; it != std::sregex_iterator(); ++it) {
         found_cluster_positions.push_back(it->position());
@@ -76,31 +44,22 @@ char *process(const char *input) {
 
     cout << "\nOriginal:\t" << new_str;
 
-    // Unload clusters from string replace with symbol
-    new_str = std::regex_replace(new_str, clusters, SYM);
+    // Unload REGEX_CLUSTERS from string replace with REGEX_SYMBOL
+
+    new_str = unload_clusters(new_str);
     cout << "\nUnloaded:\t" << new_str;
 
     // Generate Abstract String
     string abs_str = input;
-    abs_str = regex_replace(new_str, vowels, "a");
-    abs_str = regex_replace(abs_str, consonants, "b");
+    abs_str = regex_replace(new_str, REGEX_VOWELS, "a");
+    abs_str = regex_replace(abs_str, REGEX_CONSONANTS, "b");
 
 
-    // Reload clusters from vectors into string.
-    for (int i = 0; i < found_clusters.size(); i++)
-        new_str = std::regex_replace(new_str, symbol, found_clusters[i], std::regex_constants::format_first_only);
+
+
+    // Reload REGEX_CLUSTERS from vectors into string.
+    new_str = load_clusters(new_str, found_clusters);
     cout << "\nReloaded:\t" << new_str << "\n";
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -111,3 +70,13 @@ char *process(const char *input) {
     return NULL;
 }
 
+bool isVowel(char c) {
+    c = tolower(c);
+    return isalpha(c) && (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y');
+}
+
+bool isConsonant(char c) {
+    if (isalpha(c))
+        return !isVowel(c);
+    return false;
+}

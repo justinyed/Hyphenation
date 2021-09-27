d81a109125e7e96c8d0bbb9d2461dd96cbf47001
Justin Yedinak <justin.yedinak.19@cnu.edu>
2021-09-27 17:51:02 -0400

    Still need to fix e at the end case

--- hyphen.cpp
+++ hyphen.cpp
@@ -7,6 +7,8 @@ using namespace std;
 // Forward Declaration
 bool isVowel(char c);
 bool isConsonant(char c);
+string delimit_VCCV(string str);
+string delimit_VCV(string s);
 string generate_abstract(string s);
 string unload_clusters(string str);
 string load_clusters(string str, vector<string> found_clusters);
@@ -14,19 +16,20 @@ vector<std::string> generate_cluster_vector (string str);
 char * convert_to_c_string(std::string s);
 
 
+
 // CONSTANTS
+const char DELIMITER = '-';
+
 const string SYM = "*";
 const string SYMBOLIC_VOWEL = "a";
 const string SYMBOLIC_CONSONANT = "b";
 const string SYMBOLIC_PATTERN_VCV = "(" + SYMBOLIC_VOWEL+ SYMBOLIC_CONSONANT+ SYMBOLIC_VOWEL + ")";
 const string SYMBOLIC_PATTERN_VCCV = "(" + SYMBOLIC_VOWEL+ SYMBOLIC_CONSONANT+ SYMBOLIC_CONSONANT + SYMBOLIC_VOWEL + ")";
-const char DELIMITER = '-';
 
 const regex REGEX_SYMBOL("[" + SYM + "]");
 const regex REGEX_VOWELS("[aeiouy]", std::regex::icase);
 const regex REGEX_CONSONANTS("[b-df-hj-np-tv-z]|[" + SYM + "]", std::regex::icase);
-const regex REGEX_CLUSTERS("(qu)|(tr)|(br)|(st)|(sl)|(bl)|(cr)|(ph)|(ch)|(str)", std::regex::icase);
-
+const regex REGEX_CLUSTERS("(str)|(qu)|(tr)|(br)|(st)|(sl)|(bl)|(cr)|(ph)|(ch)", std::regex::icase);
 const regex REGEX_PATTERN_VCV(SYMBOLIC_PATTERN_VCV, std::regex::icase);
 const regex REGEX_PATTERN_VCCV(SYMBOLIC_PATTERN_VCCV, std::regex::icase);
 
@@ -35,23 +38,16 @@ char *process(const char *input) {
 
     string org_str = input;
     string new_str = unload_clusters(org_str);
-    string abs_str = generate_abstract(new_str);
+
     vector<std::string> clusters = generate_cluster_vector (org_str);
 
+    new_str = delimit_VCCV(new_str);
+    new_str = delimit_VCCV(new_str);
+    new_str = delimit_VCV(new_str);
+    new_str = delimit_VCV(new_str);
 
 
 
-
-    string str = abs_str;
-    vector<int> positions;
-    auto it = std::sregex_iterator(str.begin(), str.end(), REGEX_PATTERN_VCCV);
-    for (; it != std::sregex_iterator(); ++it) {
-        positions.push_back(it->position() + 2);
-    }
-
-    for (int i = 0; i < positions.size(); i++){
-        new_str.insert(positions[i], 1, DELIMITER);
-    }
 
     return convert_to_c_string(load_clusters(new_str, clusters));
 }
@@ -99,6 +95,41 @@ vector<std::string> generate_cluster_vector (string str){
 }
 
 string generate_abstract(string s) {
+//    s = regex_replace(s, regex ("(e )", std::regex::icase), "1");
     s = regex_replace(s, REGEX_VOWELS, SYMBOLIC_VOWEL);
     return regex_replace(s, REGEX_CONSONANTS, SYMBOLIC_CONSONANT);
+}
+
+string delimit_VCCV(string s) {
+    string str = generate_abstract(s);
+
+    vector<int> positions;
+    auto it = std::sregex_iterator(str.begin(), str.end(), REGEX_PATTERN_VCCV);
+
+    for (; it != std::sregex_iterator(); ++it) {
+        positions.push_back(it->position());
+    }
+
+    int j = 0;
+    for (int i = 0; i < positions.size(); i++){
+        s.insert(positions[i] + 2 + j++, 1, DELIMITER);
+    }
+    return s;
+}
+
+string delimit_VCV(string s) {
+    string str = generate_abstract(s);
+
+    vector<int> positions;
+    auto it = std::sregex_iterator(str.begin(), str.end(), REGEX_PATTERN_VCV);
+
+    for (; it != std::sregex_iterator(); ++it) {
+        positions.push_back(it->position());
+    }
+
+    int j = 0;
+    for (int i = 0; i < positions.size(); i++){
+        s.insert(positions[i] + 1 + j++, 1, DELIMITER);
+    }
+    return s;
 }

2b5868feab098f9cddad8580c67c9a688c3486e8
Justin Yedinak <justin.yedinak.19@cnu.edu>
2021-09-28 09:03:45 -0400

    Chnaged a few things

--- hyphen.cpp
+++ hyphen.cpp
@@ -5,16 +5,19 @@
 using namespace std;
 
 // Forward Declaration
-bool isVowel(char c);
-bool isConsonant(char c);
 string delimit_VCCV(string str);
-string delimit_VCV(string s);
-string generate_abstract(string s, bool isVCV=false);
-string unload_clusters(string str);
-string load_clusters(string str, vector<string> found_clusters);
-vector<std::string> generate_cluster_vector (string str);
-char * convert_to_c_string(std::string s);
 
+string delimit_VCV(string s);
+
+string generate_abstract(string s, bool isVCV = false);
+
+string unload_clusters(const string &str);
+
+string load_clusters(string str, const vector<string> &found_clusters);
+
+vector<std::string> generate_cluster_vector(string str);
+
+char *convert_to_c_string(const std::string &s);
 
 
 // CONSTANTS
@@ -23,8 +26,9 @@ const char DELIMITER = '-';
 const string SYM = "*";
 const string SYMBOLIC_VOWEL = "a";
 const string SYMBOLIC_CONSONANT = "b";
-const string SYMBOLIC_PATTERN_VCV = "(" + SYMBOLIC_VOWEL+ SYMBOLIC_CONSONANT+ SYMBOLIC_VOWEL + ")";
-const string SYMBOLIC_PATTERN_VCCV = "(" + SYMBOLIC_VOWEL+ SYMBOLIC_CONSONANT+ SYMBOLIC_CONSONANT + SYMBOLIC_VOWEL + ")";
+const string SYMBOLIC_PATTERN_VCV = "(" + SYMBOLIC_VOWEL + SYMBOLIC_CONSONANT + SYMBOLIC_VOWEL + ")";
+const string SYMBOLIC_PATTERN_VCCV =
+        "(" + SYMBOLIC_VOWEL + SYMBOLIC_CONSONANT + SYMBOLIC_CONSONANT + SYMBOLIC_VOWEL + ")";
 
 const regex REGEX_SYMBOL("[" + SYM + "]");
 const regex REGEX_VOWELS("[aeiouy]", std::regex::icase);
@@ -39,22 +43,20 @@ char *process(const char *input) {
     string org_str = input;
     string new_str = unload_clusters(org_str);
 
-    vector<std::string> clusters = generate_cluster_vector (org_str);
+    vector<std::string> clusters = generate_cluster_vector(org_str);
 
     new_str = delimit_VCCV(new_str);
     new_str = delimit_VCCV(new_str);
     new_str = delimit_VCV(new_str);
     new_str = delimit_VCV(new_str);
-
-
 
 
     return convert_to_c_string(load_clusters(new_str, clusters));
 }
 
-char* convert_to_c_string(std::string s){
+char *convert_to_c_string(const std::string &s) {
     // Dynamically allocate memory for the returned string
-    char* ptr = new char[s.size() + 1]; // +1 for terminating NUL
+    char *ptr = new char[s.size() + 1]; // +1 for terminating NUL
 
     // Copy source string in dynamically allocated string buffer
     strcpy(ptr, s.c_str());
@@ -63,37 +65,27 @@ char* convert_to_c_string(std::string s){
     return ptr;
 }
 
-bool isVowel(char c) {
-    c = tolower(c);
-    return isalpha(c) && (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y');
-}
-
-bool isConsonant(char c) {
-    if (isalpha(c))
-        return !isVowel(c);
-    return false;
-}
-
-string unload_clusters(string str){
+string unload_clusters(const string &str) {
     return std::regex_replace(str, REGEX_CLUSTERS, SYM);
 }
 
-string load_clusters(string str, vector<string> found_clusters){
-    for (int i = 0; i < found_clusters.size(); i++)
-        str = std::regex_replace(str, REGEX_SYMBOL, found_clusters[i], std::regex_constants::format_first_only);
+string load_clusters(string str, const vector<string> &found_clusters) {
+    for (auto &found_cluster: found_clusters)
+        str = std::regex_replace(str, REGEX_SYMBOL, found_cluster, std::regex_constants::format_first_only);
     return str;
 }
 
-vector<std::string> generate_cluster_vector (string str){
+vector<std::string> generate_cluster_vector(string str) {
     vector<std::string> found_clusters;
     auto it = std::sregex_iterator(str.begin(), str.end(), REGEX_CLUSTERS);
 
-    for(; it != std::sregex_iterator(); ++it) {
+    for (; it != std::sregex_iterator(); ++it) {
         found_clusters.push_back(it->str());
     }
     return found_clusters;
 }
 
+// REGEX
 // https://stackoverflow.com/questions/61017760/regex-to-match-words-followed-by-whitespace-or-punctuation
 // (?!              # begin a negative lookahead
 //  [^ .,?!;\r\n]   # match 1 char other than those in " .,?!;\r\n"
@@ -117,8 +109,8 @@ string delimit_VCCV(string s) {
     }
 
     int j = 0;
-    for (int i = 0; i < positions.size(); i++){
-        s.insert(positions[i] + 2 + j++, 1, DELIMITER);
+    for (int position: positions) {
+        s.insert(position + 2 + j++, 1, DELIMITER);
     }
     return s;
 }
@@ -134,8 +126,8 @@ string delimit_VCV(string s) {
     }
 
     int j = 0;
-    for (int i = 0; i < positions.size(); i++){
-        s.insert(positions[i] + 1 + j++, 1, DELIMITER);
+    for (int position: positions) {
+        s.insert(position + 1 + j++, 1, DELIMITER);
     }
     return s;
 }
